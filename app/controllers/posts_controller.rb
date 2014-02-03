c084e5a99d65ecae010be24902e71d5dc4f20d04
vadzim <v.for.vadja@gmail.com>
2014-02-03 14:05:43 +0300

    Added comments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
 
-  #before_filter :authenticate_user!, except => [:show, :index]
+  before_filter :authenticate_user!, except => [:show, :index]
 
 
   def new
